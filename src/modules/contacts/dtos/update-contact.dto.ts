2e64c3cd64aedef832e976d62e28da154d7adaa6
Pedro Henrique Abreu Marcusso <pedromarcusso@gmail.com>
2023-12-03 14:36:16 -0300

    fix: organizando pastas e arquivos

--- src/modules/contacts/dtos/update-contact.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateContactDTO } from './create-contact.dto';
-
-export class UpdateContactDto extends PartialType(CreateContactDTO) {}
